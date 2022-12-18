08d1573ccf7b0bf99fc6b3541dcbcfa61a80a196
MuriloMelo94 <murilomelo94@outlook.com>
2022-12-18 00:08:46 -0300

    Build :building_construction: Persistencia da entidade Tarefa e propriedades do Hibernate.

--- src/main/java/br/com/esiggroup/model/Tarefa.java
+++ src/main/java/br/com/esiggroup/model/Tarefa.java
@@ -1,17 +1,45 @@
 package br.com.esiggroup.model;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Tarefa implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 	
 	private String titulo;
 	private String descricao;
 	private String responsavel;
 	private String prioridade;
-	private String deadline;
 	
+	@Temporal(TemporalType.DATE)
+	private Date deadline;
+	
+	public Tarefa() {
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public String getTitulo() {
 		return titulo;
 	}
@@ -44,12 +72,31 @@ public class Tarefa implements Serializable{
 		this.prioridade = prioridade;
 	}
 	
-	public String getDeadline() {
+	public Date getDeadline() {
 		return deadline;
 	}
 	
-	public void setDeadline(String deadline) {
+	public void setDeadline(Date deadline) {
 		this.deadline = deadline;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		return id == other.id;
+	}
+	
+	
 	
 }
